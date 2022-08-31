94bbe38dbd725f89f78fba5931ad875f69506e4f
Betty985 <1830265753@qq.com>
2022-08-31 13:28:14 +0800

    feat(all): 项目初始化

---
+++ .prettierrc.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  bracketSpacing: false,
+  singleQuote: true,
+  jsxBracketSameLine: true,
+  trailingComma: 'es5',
+  printWidth: 80,
+  parser: 'babel',
+  semi: false,
+  singleQuote: true,
+  printWidth: 80,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  overrides: [
+    {
+ 
+      options: {
+        trailingComma: 'all',
+      },
+    },
+  ],
+};
